52552c99151b3968bc09b1b4856ea95ea6da6368
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2019-05-24 12:34:58 -0500

    Update documentation for core and pycom

--- apidoc/core-apidoc/search/all_d.js
+++ apidoc/core-apidoc/search/all_d.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['riaps_20framework_20in_20c_2b_2b',['RIAPS Framework in C++',['../index.html',1,'']]],
   ['recv',['Recv',['../classriaps_1_1ports_1_1PeriodicTimer.html#afe543d886ebd25bdcebcb336c1cccc87',1,'riaps::ports::PeriodicTimer::Recv()'],['../classriaps_1_1ports_1_1RecvPort.html#a2e0cb84c825f22523e343c4e5696a004',1,'riaps::ports::RecvPort::Recv()']]],
   ['recvport',['RecvPort',['../classriaps_1_1ports_1_1RecvPort.html',1,'riaps::ports::RecvPort'],['../classriaps_1_1ports_1_1RecvPort.html#ad951c7619c70ca326a0e85d4e88ca1eb',1,'riaps::ports::RecvPort::RecvPort()']]],
   ['recvquery',['RecvQuery',['../classriaps_1_1ports_1_1QueryPort.html#a456cc81940dc3cda7612a4d7208c0154',1,'riaps::ports::QueryPort']]],
--- apidoc/core-apidoc/search/pages_0.js
+++ apidoc/core-apidoc/search/pages_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['readme',['README',['../md_examples_group_features_ActiveReplica_README.html',1,'']]],
-  ['riaps_20framework_20in_20c_2b_2b',['RIAPS Framework in C++',['../md_README.html',1,'']]]
+  ['riaps_20framework_20in_20c_2b_2b',['RIAPS Framework in C++',['../index.html',1,'']]]
 ];
